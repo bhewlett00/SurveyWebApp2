7a69d27b4746d77452a91cfed76ab23b35295689
Bri Hewlett <hewlettb@chc.edu>
2020-10-27 17:02:20 -0400

    Recreated Survey web app. Created survey controller.

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,24 @@
+class SurveysController < ApplicationController
+	def index
+		@surveys = Survey.all
+		@scale = [1, 2, 3, 4, 5]
+	end
+
+	def new
+	end 
+
+	def create
+		survey = Survey.new(survey_params)
+		if survey.save
+			redirect_to "/surveys"
+		else
+			flash[:errors] = survey.errors.full_messages
+			redirect_to "/surveys/new"
+		end
+	end
+
+	private
+	def survey_params
+		params.require(:survey).permit(:survey)
+	end
+end
