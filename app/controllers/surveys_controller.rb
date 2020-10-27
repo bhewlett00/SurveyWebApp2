cb264a10c73e24981b1129df27b20263a65759df
Bri Hewlett <hewlettb@chc.edu>
2020-10-27 17:16:30 -0400

    Added Validation

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -19,6 +19,6 @@ class SurveysController < ApplicationController
 
 	private
 	def survey_params
-		params.require(:survey).permit(:survey)
+		params.require(:survey).permit(:survey_name)
 	end
 end
